098156135989d48238442ca05413bff9000abbd8
Jesse Conway <jessicaeconway12@gmail.com>
2019-10-16 17:10:07 -0400

    toggle classes and loading screen

--- src/index.js
+++ src/index.js
@@ -13,17 +13,73 @@ window.addEventListener('DOMContentLoaded', () => {
         .attr("fill", "white");
 
     const input = document.getElementById('toggle-button');
+    const formObj = {};
+
     input.addEventListener('click', (e) => {
         const inputs = document.getElementsByClassName('inputs');
-        const formObj = {};
         for (let i = 0; i < inputs.length; i++) {
             formObj[inputs[i].value] = inputs[i].checked
         }
+        const loader = document.getElementsByClassName('loaded')[0];
+        loader.classList.remove('loaded');
+        loader.classList.add('loader');
+
+        getAreas().then(urbanAreas => { 
+         const dUrbanAreas = [];
+         for(let i = 0; i < urbanAreas.length; i++) {
+            let obj = Object.assign({}, urbanAreas[i]);
+            let scores = obj.scoresArr;
+            let newScores = [];
+
+            for (let j = 0; j < scores.length; j++) {
+                let scoreObj = Object.assign({}, scores[j]);
+                let prevScores = newScores.slice(0, j);
+                let sum = 0;
+                prevScores.forEach(e => {
+                    sum = sum + e.score_out_of_10 * 5;
+                })
+                scoreObj['startHeight'] = sum;
+
+                //if not selected for, make score 0
+                if (formObj[scoreObj.name] == false) {
+                    scoreObj.score_out_of_10 = 0;
+                } 
+                 
+                newScores.push(scoreObj);
+            }
+            obj['scoresArr'] = newScores;
+            dUrbanAreas.push(obj);
+            }  
+            // debugger
+            svg.selectAll('rect').remove();
+            var selection2 = svg.selectAll(".series2")
+                .data(dUrbanAreas)
+                .enter().append("g")
+                .attr('class', function(d) {return `series ${d.name}`;})
+                .attr("transform", function (d, i) {
+                    return "translate(" + i * 30 + ",0)";
+                })
+                .text(function (d) { return d.name });
+        
+            selection2.selectAll("rect")
+              .data(function (d) { return d.scoresArr; })
+              .enter().append("rect")
+              .attr('id', function (s) { return s.name })
+              .attr("width", 20)
+              .attr("y", function (s) { return s.startHeight })
+              .attr("height", function (s) { return s.score_out_of_10 * 5; })
+              .style("fill", function (s) { return s.color });
+
+
+            const loader = document.getElementsByClassName('loader')[0];
+            loader.classList.remove('loader');
+            loader.classList.add('loaded');
+         });
     })
 
-    getAreas().then(urbanAreas => { 
+    getAreas().then(urbanAreas => {
         const dUrbanAreas = [];
-        for(let i = 0; i < urbanAreas.length; i++) {
+        for (let i = 0; i < urbanAreas.length; i++) {
             let obj = Object.assign({}, urbanAreas[i]);
             let scores = obj.scoresArr;
             let newScores = [];
@@ -35,23 +91,23 @@ window.addEventListener('DOMContentLoaded', () => {
                 prevScores.forEach(e => {
                     sum = sum + e.score_out_of_10 * 5;
                 })
-
                 scoreObj['startHeight'] = sum;
+
                 newScores.push(scoreObj);
             }
             obj['scoresArr'] = newScores;
             dUrbanAreas.push(obj);
-        }  
-        debugger
+        }
+        // debugger
         var selection = svg.selectAll(".series")
             .data(dUrbanAreas)
             .enter().append("g")
-            .attr("class", "series")
-            .attr('class', function(d) {return d.name})
+            .attr('class', function (d) { return `series ${d.name}`; })
             .attr("transform", function (d, i) {
                 return "translate(" + i * 30 + ",0)";
-            });
-        
+            })
+            .text(function (d) { return d.name });
+
         selection.selectAll("rect")
             .data(function (d) { return d.scoresArr; })
             .enter().append("rect")
@@ -60,5 +116,10 @@ window.addEventListener('DOMContentLoaded', () => {
             .attr("y", function (s) { return s.startHeight })
             .attr("height", function (s) { return s.score_out_of_10 * 5; })
             .style("fill", function (s) { return s.color });
+
+        const loader = document.getElementsByClassName('loader')[0];
+        loader.classList.remove('loader');
+        loader.classList.add('loaded');
     });
 })
+
