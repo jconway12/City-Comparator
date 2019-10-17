a22622903fd7ca652f2496d3e30b625b290031cc
Jesse Conway <jessicaeconway12@gmail.com>
2019-10-17 14:43:58 -0400

    Fix styling, add all-cities button

--- src/index.js
+++ src/index.js
@@ -4,6 +4,26 @@ import {getUrbanAreasXML} from './scripts/life_data';
 import {getAreas} from './scripts/life_data_main';
 
 window.addEventListener('DOMContentLoaded', () => {
+    const inputDivs = document.getElementsByClassName('input-div');
+    for(let i = 0; i < inputDivs.length; i++) {
+        const slide = inputDivs[i].childNodes[2];
+        const minVal = slide.value;
+        const label = document.createTextNode(`Min: ${minVal}`);
+        const labelDiv = document.createElement("div");
+        labelDiv.appendChild(label);
+        inputDivs[i].appendChild(labelDiv);
+
+        inputDivs[i].addEventListener('mousemove', e => {
+            const slide = e.currentTarget.childNodes[2];
+            const minVal = slide.value;
+            const labelD = e.currentTarget.childNodes[3];
+            labelD.innerHTML = "";
+            const newText = document.createTextNode(`Min: ${minVal}`);
+            labelD.appendChild(newText);
+            e.currentTarget.appendChild(labelD);
+        })
+    }
+
     const root = document.getElementById('app');
     var svg = d3.select("#svg-container")
         .append("svg")
@@ -192,6 +212,86 @@ window.addEventListener('DOMContentLoaded', () => {
             loader.classList.add('loaded');
         });
     })
+
+    document.getElementById('all-cities').addEventListener('click', (e) => {
+        const search = document.getElementById('search-bar');
+        search.value = "";
+
+        const loader = document.getElementsByClassName('loaded')[0];
+        loader.classList.remove('loaded');
+        loader.classList.add('loader');
+
+        getAreas().then(urbanAreas => {
+            const dUrbanAreas = [];
+            for (let i = 0; i < urbanAreas.length; i++) {
+                let obj = Object.assign({}, urbanAreas[i]);
+                let scores = obj.scoresArr;
+                let newScores = [];
+
+                    for (let j = 0; j < scores.length; j++) {
+                        let scoreObj = Object.assign({}, scores[j]);
+                        let prevScores = newScores.slice(0, j);
+                        let sum = 0;
+                        prevScores.forEach(e => {
+                            sum = sum + e.score_out_of_10 * 5;
+                        })
+                        scoreObj['startHeight'] = sum;
+
+                        newScores.push(scoreObj);
+                    }
+                    obj['scoresArr'] = newScores;
+                    dUrbanAreas.push(obj);
+            }
+
+            // debugger
+            svg.selectAll('rect').remove();
+            var selection2 = svg.selectAll(".series2")
+                .data(dUrbanAreas)
+                .enter().append("g")
+                .attr('class', function (d) { return `series ${d.name}`; })
+                .attr("transform", function (d, i) {
+                    return "translate(" + i * 30 + ",0)";
+                })
+                .text(function (d) { return d.name })
+                .on('mouseover', e => {
+                    const tooltip = document.getElementById("tooltip");
+                    tooltip.innerHTML = "";
+                    // tooltip.setAttribute('style', `height: 500px; width: 100px;`);
+
+                    const city = e.name;
+                    const scores = e.scoresArr;
+                    const newDiv = document.createElement("div");
+                    const newContent = document.createTextNode(`${city}`);
+                    newDiv.appendChild(newContent);
+                    tooltip.appendChild(newDiv);
+
+                    const newUl = document.createElement("ul");
+                    for (let i = 0; i < scores.length; i++) {
+                        const newLi = document.createElement('li');
+                        const score = Number(Math.round(scores[i].score_out_of_10 + 'e' + 2) + 'e-' + 2);
+                        const listItem = document.createTextNode(`${scores[i].name}: ${score}`);
+                        newLi.appendChild(listItem)
+                        newUl.appendChild(newLi);
+                    }
+                    tooltip.appendChild(newUl);
+                });
+
+            selection2.selectAll("rect")
+                .data(function (d) { return d.scoresArr; })
+                .enter().append("rect")
+                .attr('id', function (s) { return s.name })
+                .attr("width", 20)
+                .attr("y", function (s) { return s.startHeight })
+                .attr("height", function (s) { return s.score_out_of_10 * 5; })
+                .style("fill", function (s) { return s.color });
+
+
+            const loader = document.getElementsByClassName('loader')[0];
+            loader.classList.remove('loader');
+            loader.classList.add('loaded');
+        });
+    })
+
 
     getAreas().then(urbanAreas => {
         const dUrbanAreas = [];
